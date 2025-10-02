8bd62ac77b0201640f896deb954a58fce4a5d52f
MatthewPhan <Matthewminhphan@gmail.com>
2025-10-02 16:06:30 -0700

    cant find

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,6 +34,9 @@ const users = {
   ]
 };
 
+
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -43,6 +46,20 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   res.send(users);
 });
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 
 app.listen(port, () => {
   console.log(
