c31e497b8ff45a0aba793b170a79b4e97b2e46ea
MatthewPhan <Matthewminhphan@gmail.com>
2025-10-14 22:14:47 -0700

    fix

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -72,5 +72,5 @@ app.delete("/users/:id", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`✅ Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
