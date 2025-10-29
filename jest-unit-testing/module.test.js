6d37dbc359c70b2376a6c9d33577e0ca877c0b62
MatthewPhan <Matthewminhphan@gmail.com>
2025-10-28 18:47:08 -0700

    module-test-cases

--- jest-unit-testing/module.test.js
+++ jest-unit-testing/module.test.js
@@ -6,3 +6,36 @@ test('Testing sum -- success', () => {
   expect(got).toBe(expected);
 });
 
+
+test('Testing div -- success', () => {
+  const expected = 5;
+  const got = mut.div(10, 2);
+  expect(got).toBe(expected);
+});
+
+test('Testing div -- divide by 0 should be Infinity', () => {
+  const got = mut.div(5, 0);
+  expect(got).toBe(Infinity);
+});
+
+test('Testing div -- negative numbers', () => {
+  const expected = -2;
+  const got = mut.div(10, -5);
+  expect(got).toBe(expected);
+});
+
+test('containsNumbers -- string with numbers', () => {
+  const got = mut.containsNumbers('abc123');
+  expect(got).toBe(true);
+});
+
+test('containsNumbers -- string with no numbers', () => {
+  const got = mut.containsNumbers('abcdef');
+  expect(got).toBe(false);
+});
+
+//this testcase should catch the bug, returning false
+test('containsNumbers -- string with spaces or symbols only', () => {
+  const got = mut.containsNumbers('   ');
+  expect(got).toBe(false); 
+});
